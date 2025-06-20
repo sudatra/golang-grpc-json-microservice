354427297bd2b49824904676f84b94621493711a
sudatra <mail2sudatra@gmail.com>
2025-06-20 20:55:32 +0530

    refactor

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
-	"log"
-
-	"github.com/sudatra/golang-grpc-json-microservice.git/client"
 )
 
 func main() {
-	client := client.New("http://localhost:3000");
-	price, err := client.FetchPrice(context.Background(), "ET");
-	if err != nil {
-		log.Fatal(err);
-	}
+	// client := client.New("http://localhost:3000");
+	// price, err := client.FetchPrice(context.Background(), "ET");
+	// if err != nil {
+	// 	log.Fatal(err);
+	// }
 
-	fmt.Printf("%+v\n", price);
-	return;
+	// fmt.Printf("%+v\n", price);
+	// return;
 
 	listenAddr := flag.String("listenaddr", ":3000", "listen address the service is running on");
 	flag.Parse();
